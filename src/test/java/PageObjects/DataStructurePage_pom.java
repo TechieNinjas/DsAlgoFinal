471b81b757235a83b95b8202d776323f0b92f469
bhavanikammila <bhavanikammila@gmail.com>
2023-11-07 17:39:49 -0600

    Changes done in module spaces to Branch_Bhavani

--- src/test/java/PageObjects/DataStructurePage_pom.java
+++ src/test/java/PageObjects/DataStructurePage_pom.java
@@ -6,17 +6,17 @@ import Utilities.BaseClass;
 
 public class DataStructurePage_pom extends BaseClass {
 
-	By DatastructuresGetStrtd=By.xpath("//div[@class='row row-cols-1 row-cols-md-3 g-4']//div[1]//div[1]//div[1]//a");
-	By TimeComplexity=By.className("list-group-item");	
+	By DatastructuresGetStrtd = By.xpath("//div[@class='row row-cols-1 row-cols-md-3 g-4']//div[1]//div[1]//div[1]//a");
+	By TimeComplexity = By.className("list-group-item");
 
-	
 	public DataStructuresPage DataStructuresGetStrtd() {
 		driver.findElement(DatastructuresGetStrtd).click();
 		return new DataStructuresPage();
 	}
+
 	public DataStructurePage_pom TimeComplexity() {
 		driver.findElement(TimeComplexity).click();
 		return this;
 	}
-	
+
 }
