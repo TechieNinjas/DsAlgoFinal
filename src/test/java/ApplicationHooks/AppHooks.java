72b2911de932d660e7f41439b94fc39900eabff1
priyackk <133934444+priyackk@users.noreply.github.com>
2023-11-07 11:44:07 -0800

    Removed commented lines from common files to branch_priya.

--- src/test/java/ApplicationHooks/AppHooks.java
+++ src/test/java/ApplicationHooks/AppHooks.java
@@ -9,43 +9,14 @@ import io.cucumber.java.Scenario;
 public class AppHooks extends BaseClass {
 
 	@Before
-	public static void beforeScenario() throws Throwable{//(Scenario scenario) { // browserSetup()
-//		Get browser Type from config file
-//		LoggerLoad.info("Loading Config file");
-//		ConfigReader.loadConfig();
-//		String browser = ConfigReader.getBrowserType();
-//		Initialize driver from BaseClass 
-//		LoggerLoad.info("Initializing driver for:"+browser);	
-		
-		BaseClass.openDriver(ConfigReader.getBrowserType());
-		
+	public static void beforeScenario() throws Throwable{
+		BaseClass.openDriver(ConfigReader.getBrowserType());	
 	}
 
 	@After
-	public void afterScenario(Scenario scenario) { // teardown()
-		// checking to see if scenario has failed
-
-		/*
-		 * if (scenario.isFailed()) { final byte[] screenshot = ((TakesScreenshot)
-		 * BaseClass.getDriver()).getScreenshotAs(OutputType.BYTES);
-		 * 
-		 * scenario.attach(screenshot, "image/png", scenario.getName()); }
-		 */
-		BaseClass.closeDriver();
+	public void afterScenario(Scenario scenario) { 
+				BaseClass.closeDriver();
 	}
 
 }
 
-/*
- * @Before("@DataStructure1") public void getUrlForDataStr() {
- * 
- * driver.get(homePageUrl);
- * 
- * }
- * 
- * @Before("@DataStructure2") public void getUrlForNDataStr() {
- * 
- * driver.get(homePageUrl);
- * 
- * }
- */
