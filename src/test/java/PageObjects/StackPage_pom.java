db8a79f8e8bc60b96037d6035d06124fb789f40f
bhavanikammila <bhavanikammila@gmail.com>
2023-11-15 10:26:55 -0600

    Added run.bat to branch_Bhavani

--- src/test/java/PageObjects/StackPage_pom.java
+++ src/test/java/PageObjects/StackPage_pom.java
@@ -11,22 +11,22 @@ public class StackPage_pom extends BaseClass {
 	By StackApplications = By.xpath("//div[@class='row']//div[1]/div/li[3]/a");
 
 	public StackPage StackGetStrtdBtn() {
-		driver.findElement(By.xpath("//div[@class='row row-cols-1 row-cols-md-3 g-4']//div[4]//a")).click();
+		driver.findElement(StackGetStrtdBtn).click();
 		return new StackPage();
 	}
 
 	public OperationsinStack OperationsinStackBtn() {
-		driver.findElement(By.xpath("//a[contains(text(), 'Operations in ')]")).click();
+		driver.findElement(OperationsinStack).click();
 		return new OperationsinStack();
 	}
 
 	public StackImplementation StackImplementationBtn() {
-		driver.findElement(By.xpath("//div[@class='col-2']//div/li[2]/a")).click();
+		driver.findElement(StackImplemantation).click();
 		return new StackImplementation();
 	}
 
 	public StackApplications StackApplicationsBtn() {
-		driver.findElement(By.xpath("//div[@class='row']//div[1]/div/li[3]/a"));
+		driver.findElement(StackApplications).click();
 		return new StackApplications();
 	}
 
